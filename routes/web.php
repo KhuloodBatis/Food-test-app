032eb19c1a87a2cd03b1571d2fbce353312c4409
KhuloodBatis <k.batis@bimventures.com>
2022-12-01 13:48:39 +0300

    test-search

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SearchProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('search');
-});
+Route::get('/', [SearchProductsController::class, 'index']);
 
 Route::get('/cart', function () {
     return view('cart');
--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -5,18 +5,67 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class SearchTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use DatabaseTransactions;
+    /** @test */
+    public function food_search_page_is_accessible()
     {
+        $this->get('/')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function food_search_page_has_all_the_required_page_data()
+    {
+        // Arrange
+        Product::factory()->count(3)->create();
+
+        // Act
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // Assert
+        $items = Product::get();
+
+        $response->assertViewIs('search')->assertViewHas('items', $items);
+    }
+
+    /** @test */
+    public function food_search_page_shows_the_items()
+    {
+        Product::factory()->count(3)->create();
+
+        $items = Product::get();
+
+        $this->get('/')
+            ->assertSeeInOrder([
+                $items[0]->name,
+                $items[1]->name,
+                $items[2]->name,
+            ]);
+    }
+
+    /** @test */
+    public function food_can_be_searched_given_a_query()
+    {
+        Product::factory()->create([
+            'name' => 'Taco'
+        ]);
+        Product::factory()->create([
+            'name' => 'Pizza'
+        ]);
+        Product::factory()->create([
+            'name' => 'BBQ'
+        ]);
+
+        $this->get('/?query=bbq')
+            ->assertSee('BBQ')
+            ->assertDontSeeText('Pizza')
+            ->assertDontSeeText('Taco');
+
+            $this->get('/')->assertSeeInOrder(['Taco', 'Pizza', 'BBQ']);
     }
 }
